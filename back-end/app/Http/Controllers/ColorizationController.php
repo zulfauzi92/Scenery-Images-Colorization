d7e229595689a26fff371974f46720bfe497bb3a
zulfauzi92 <hatake.kakasi956@gmail.com>
2022-07-07 22:38:22 +0700

    show all result colorization
    add index function in colorization controller and additional route

--- back-end/app/Http/Controllers/ColorizationController.php
+++ back-end/app/Http/Controllers/ColorizationController.php
@@ -12,6 +12,36 @@ use Validator;
 
 class ColorizationController extends Controller
 {
+
+    public function index(){
+        
+        $temp = Colorization::all();
+        
+        $results = array();
+
+        foreach ($temp as $key) {
+            $colored = DB::table('colored')
+            ->where('id', 'like', $key->colored_id)
+            ->first();
+
+            $colorless = DB::table('colorless')
+            ->where('id', 'like', $key->colorless_id)
+            ->first();
+
+            $union['colorless_id'] = $colorless->id;
+            $union['colorless_link'] = $colorless->colorless_link;
+            $union['colored_id'] = $colored->id;
+            $union['colored_link'] = $colored->colored_link;
+
+            array_push($results, $union);
+
+        
+        }
+
+        return response()->json(compact('results'));
+
+    }
+
     public function store(Request $request){
         $validator = Validator::make($request->all(), [
             'colorless_id' => 'required',
